266377e50bebe7afc00db6c9f4bdc43a4bc48c62
koobcbc <koobcbc@gmail.com>
2025-11-11 21:30:59 +0000

    Deploying to gh-pages from @ koobcbc/koobcbc.github.io@feeac294c8339c1af66ba706899ded90cb520d7c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "koobcbc/personal-page",
+    "data-repo": "koobcbc/koobcbc.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
